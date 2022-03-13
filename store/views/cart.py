81b854faabae038f7d876152b2be401897b1a30e
peter88tom <peter88tom@gmail.com>
2022-03-13 12:27:32 +0200

    Add and remove item(s) from cart

--- store/views/cart.py
+++ store/views/cart.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render , redirect
 
-from django.contrib.auth.hashers import  check_password
+from django.contrib.auth.hashers import check_password
 from store.models.customer import Customer
-from django.views import  View
+from django.views import View
 from store.models.product import Products
 
+
 class Cart(View):
-    def get(self , request):
+    def post(self, request):
+      pass
+        # product = request.POST.get('product')
+        # remove = request.POST.get('remove')
+        # cart = request.session.get('cart')
+        #
+        # if cart:
+        #     quantity = cart.get(product)
+        #     if quantity:
+        #         if remove:
+        #             if quantity<=1:
+        #                 cart.pop(product)
+        #             else:
+        #                 cart[product]  = quantity-1
+        #         else:
+        #             cart[product]  = quantity+1
+        #
+        #     else:
+        #         cart[product] = 1
+        # else:
+        #     cart = {}
+        #     cart[product] = 1
+        #
+        # request.session['cart'] = cart
+        # return redirect(request.get_full_path())
+
+    def get(self, request):
         ids = list(request.session.get('cart').keys())
         products = Products.get_products_by_id(ids)
         print(products)
-        return render(request , 'cart.html' , {'products' : products} )
+        return render(request, 'cart.html', {'products': products})
 
