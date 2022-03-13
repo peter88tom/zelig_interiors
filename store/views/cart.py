0c50ea39ebabc4750114b6a6cc234615b1d18d11
peter88tom <peter88tom@gmail.com>
2022-03-14 00:49:09 +0200

    Show list of item added to the cart

--- store/views/cart.py
+++ store/views/cart.py
@@ -8,30 +8,29 @@ from store.models.product import Products
 
 class Cart(View):
     def post(self, request):
-      pass
-        # product = request.POST.get('product')
-        # remove = request.POST.get('remove')
-        # cart = request.session.get('cart')
-        #
-        # if cart:
-        #     quantity = cart.get(product)
-        #     if quantity:
-        #         if remove:
-        #             if quantity<=1:
-        #                 cart.pop(product)
-        #             else:
-        #                 cart[product]  = quantity-1
-        #         else:
-        #             cart[product]  = quantity+1
-        #
-        #     else:
-        #         cart[product] = 1
-        # else:
-        #     cart = {}
-        #     cart[product] = 1
-        #
-        # request.session['cart'] = cart
-        # return redirect(request.get_full_path())
+        product = request.POST.get('product')
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                    if quantity<=1:
+                        cart.pop(product)
+                    else:
+                        cart[product]  = quantity-1
+                else:
+                    cart[product]  = quantity+1
+
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+
+        request.session['cart'] = cart
+        return redirect(request.get_full_path())
 
     def get(self, request):
         ids = list(request.session.get('cart').keys())
--- store/views/cart_items.py
+++ store/views/cart_items.py
@@ -2,6 +2,9 @@ from store.models.product import Products
 
 
 def get(request):
+    cart = request.session.get('cart')
+    if not cart:
+      request.session['cart'] = {}
     ids = list(request.session.get('cart').keys())
     products = Products.get_products_by_id(ids)
     print(products)
