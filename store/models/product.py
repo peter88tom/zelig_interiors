39a42027382e12ce662b543cfa472581c4370110
peter88tom <peter88tom@gmail.com>
2022-03-14 05:19:23 +0200

    Add featured,best sales and new product feature on home page

--- store/models/product.py
+++ store/models/product.py
@@ -1,15 +1,21 @@
 from django.db import models
 from .category import Category
+
+
 class Products(models.Model):
     name = models.CharField(max_length=60)
-    price= models.IntegerField(default=0)
-    category= models.ForeignKey(Category,on_delete=models.CASCADE,default=1 )
-    description= models.CharField(max_length=250, default='', blank=True, null= True)
-    image= models.ImageField(upload_to='uploads/products/')
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.TextField(default='-', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/%Y/%m/%d/')
+    is_featured = models.BooleanField(default=False)
+    is_new = models.BooleanField(default=False)
+    is_best_sale = models.BooleanField(default=False)
 
     @staticmethod
     def get_products_by_id(ids):
-        return Products.objects.filter (id__in=ids)
+        return Products.objects.filter(id__in=ids)
+
     @staticmethod
     def get_all_products():
         return Products.objects.all()
@@ -17,6 +23,18 @@ class Products(models.Model):
     @staticmethod
     def get_all_products_by_categoryid(category_id):
         if category_id:
-            return Products.objects.filter (category=category_id)
+            return Products.objects.filter(category=category_id)
         else:
-            return Products.get_all_products();
+            return Products.get_all_products()
+
+    @staticmethod
+    def get_all_feature_products():
+        return Products.objects.filter(is_featured=True)[:3]
+
+    @staticmethod
+    def get_new_product():
+        return Products.objects.get(is_new=True)
+
+    @staticmethod
+    def get_best_sale_product():
+        return Products.objects.filter(is_best_sale=True).order_by("-id")[:1]
--- store/views/home.py
+++ store/views/home.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render , redirect , HttpResponseRedirect
 from store.models.product import Products
 from store.models.category import Category
+from store.models.orders import Order
 from django.views import View
 
 
 # Create your views here.
 class Index(View):
-
-    def post(self , request):
+    def post(self, request):
         pass
 
-    def get(self , request):
-        print(request.get_full_path())
+    def get(self, request):
         return HttpResponseRedirect(f'/home{request.get_full_path()[1:]}')
 
 
@@ -33,6 +32,24 @@ def home(request):
     #
     # print('you are : ', request.session.get('email'))
     # #return render(request, 'store/index.html', data)
-    return render(request, 'store/index.html')
+    # ToDo: Featured products
+    """
+      1. Products that are featured are best sales products selected randomly based on the order
+      2. If nothing then get 3 products that are marked as best sale in product table
+    """
+    featured = Products.get_all_feature_products()
+
+    # new product
+    new = Products.get_new_product()
+
+    # best sale
+    best_sales = Products.get_best_sale_product()
+
+    data = {
+      "featured":featured,
+      'new':new,
+      'best_sales':best_sales,
+    }
+    return render(request, 'store/index.html', data)
 
 
